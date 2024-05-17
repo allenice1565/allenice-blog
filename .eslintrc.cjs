91f6b06af0abc228c697b8559e100c800601d144
allen <allenice1565@gmail.com>
2024-05-17 15:42:38 +0800

    更新eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,12 @@ module.exports = {
   env: {
     browser: true,
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   extends: ['plugin:vue/vue3-recommended', '.eslintrc-auto-import.json', 'prettier'],
   rules: {
     'vue/multi-word-component-names': 'off',
