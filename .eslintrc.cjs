d5935b583770140cfb51a5ad2aca043e42b18329
allen <allenice1565@gmail.com>
2024-05-17 15:10:21 +0800

    格式化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
+  env: {
+    browser: true,
+  },
   extends: ['plugin:vue/vue3-recommended', '.eslintrc-auto-import.json', 'prettier'],
   rules: {
     'vue/multi-word-component-names': 'off',
+    semi: [2, 'never'],
   },
 }
--- src/pages/editor/index.vue
+++ src/pages/editor/index.vue
@@ -4,50 +4,49 @@
       <Toolbar
         style="border-bottom: 1px solid #ccc"
         :editor="editorRef"
-        :defaultConfig="toolbarConfig"
+        :default-config="toolbarConfig"
         :mode="mode"
       />
       <Editor
-        style="height: 500px; overflow-y: hidden"
         v-model="valueHtml"
-        :defaultConfig="editorConfig"
+        style="height: 500px; overflow-y: hidden"
+        :default-config="editorConfig"
         :mode="mode"
-        @onCreated="handleCreated"
+        @on-created="handleCreated"
       />
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { useTitle } from '@vueuse/core';
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
-import '@wangeditor/editor/dist/css/style.css'; // 引入 css
-import { onBeforeUnmount, onMounted, ref, shallowRef } from 'vue';
+import { useTitle } from '@vueuse/core'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-useTitle('写文章', { titleTemplate: '%s | Allenice' });
+useTitle('写文章', { titleTemplate: '%s | Allenice' })
 // 编辑器实例，必须用 shallowRef
-const editorRef = shallowRef();
+const editorRef = shallowRef()
 
 // 内容 HTML
-const valueHtml = ref('<p>hello</p>');
+const valueHtml = ref('<p>hello</p>')
 
 // 模拟 ajax 异步获取内容
 onMounted(() => {
   setTimeout(() => {
-    valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>';
-  }, 1500);
-});
+    valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
+  }, 1500)
+})
 
-const toolbarConfig = {};
-const editorConfig = { placeholder: '请输入内容...' };
+const toolbarConfig = {}
+const editorConfig = { placeholder: '请输入内容...' }
 
 // 组件销毁时，也及时销毁编辑器
 onBeforeUnmount(() => {
-  const editor = editorRef.value;
-  if (editor == null) return;
-  editor.destroy();
-});
+  const editor = editorRef.value
+  if (editor == null) return
+  editor.destroy()
+})
 
 const handleCreated = (editor) => {
-  editorRef.value = editor; // 记录 editor 实例，重要！
-};
+  editorRef.value = editor // 记录 editor 实例，重要！
+}
 </script>
